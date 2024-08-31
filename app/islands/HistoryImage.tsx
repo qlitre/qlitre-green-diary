25d67338d57876353c1b861705c9b828045b48be
qlitre <kuri0403@gmail.com>
2024-09-01 01:16:40 +0900

    画像の拡大機能

--- app/islands/HistoryImage.tsx
+++ app/islands/HistoryImage.tsx
@@ -5,14 +5,34 @@ type Props = {
     images: MicroCMSImage[]
 }
 
-
 export default function HistoryImage({ images }: Props) {
-    const [count, setCount] = useState(0)
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
     return (
-        <img
-            alt='alt'
-            src={images[0].url}
-            className="w-24 h-32 sm:w-48 sm:h-64 rounded-md object-cover object-center"
-        />
-    )
+        <>
+            <img
+                alt='alt'
+                src={images[0].url}
+                className="w-24 h-32 sm:w-48 sm:h-64 rounded-md object-cover object-center cursor-pointer"
+                onClick={openModal}
+            />
+            {isOpen && (
+                <div
+                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 cursor-pointer"
+                    onClick={closeModal}
+                >
+                    <div className="bg-white p-4 rounded-md" onClick={(e) => e.stopPropagation()}>
+                        <img
+                            alt='alt'
+                            src={images[0].url}
+                            className="max-w-[90vw] max-h-[90vh] rounded-md object-contain"
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
